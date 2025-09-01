fbd120c2b00cb84bb72cc87b76d62b58ad977292
AaronRamirez05 <aaronramirez879@yahoo.com>
2025-09-01 18:18:57 -0500

    updated API for powerlifting application

--- PowerLifter/obj/Release/net7.0/PowerLifter.AssemblyInfo.cs
+++ PowerLifter/obj/Release/net7.0/PowerLifter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PowerLifter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a3ed196bc22ca98869683691f6d5fd1c4aaf95")]
 [assembly: System.Reflection.AssemblyProductAttribute("PowerLifter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PowerLifter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Powerlifting.Service/ServiceFunctions.cs
+++ Powerlifting.Service/ServiceFunctions.cs
@@ -37,5 +37,19 @@ namespace Powerlifting.Service
             string sql = @"INSERT INTO [dbo].[UserXProgramId] ([UserId],[Name]) Values (@UserId,@Name)";
             return _db.SaveData(sql, request);
         }
+
+        public Task InsertWorkoutDetails(WorkoutDetails request)                //Using this for now 
+        {
+            string sql = @"INSERT INTO [dbo].[WorkoutDetails] ([Day],[WorkoutName],[SRRWeek1],[SRRWeek2],[SRRWeek3],[SRRWeek4],[ProgramName],[UserId],[workoutKey]) Values (@Day,@WorkoutName,@SRRWeek1,@SRRWeek2,@SRRWeek3,@SRRWeek4,@ProgramName,@UserId,NEWID())";
+            return _db.SaveData(sql, request);
+        }
+
+        public Task<List<WorkoutDetails>> GetSpecificUserWorkouts(string UserId, string pname)    //Using this for now
+        {
+            string sql = "SELECT [Day],[WorkoutName],[SRRWeek1],[SRRWeek2],[SRRWeek3],[SRRWeek4],[ProgramName],[UserId] FROM [db_a94b6f_powerlift].[dbo].[WorkoutDetails] where UserId= '" + UserId + "' AND ProgramName = '" + pname + "'  "; 
+            return _db.LoadData<WorkoutDetails, dynamic>(sql, new { });
+
+        }
+
     }
 }
